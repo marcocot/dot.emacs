6206c8438c5d9799d71865759cb9900cd2251eff
Marco Cotrufo <marco.cotrufo@deltatre.it>
2016-10-27 16:18:46 +0200

    JS better config

--- core/marco-js.el
+++ core/marco-js.el
@@ -15,8 +15,14 @@
 ;;
 ;;; Code:
 
-(use-package tern :ensure t)
-(use-package company-tern :ensure t)
+(use-package tern
+  :commands tern-mode
+  :config
+  (add-hook 'js2-mode-hook (lambda() (tern-mode t)))
+  (with-eval-after-load "company"
+    (use-package company-tern
+      :config
+      (add-to-list 'company-backends 'company-tern))))
 
 (use-package js2-mode
   :mode (("\\.js$" . js2-mode)
@@ -30,15 +36,27 @@
   (add-to-list 'company-backends 'company-tern)
   (add-hook 'js2-mode-hook 'mc/js2-mode-hook)
   :config
-  (setq js2-basic-indent 2
-        js2-basic-offset 2
-        js2-auto-indent-p t
-        js2-cleanup-whitespace t
-        js2-enter-indents-newline t
-        js2-indent-on-enter-key t
-        js2-global-externs (list "window" "module" "require" "assert" "expect" "jQuery" "$")))
+  (use-package js2-refactor
+    :commands (js2r-add-keybindings-with-prefix)
+    :init
+    (add-hook 'js2-mode-hook #'js2-refactor-mode)
+    (js2r-add-keybindings-with-prefix "C-c C-m"))
+  (setq
+   js2-mode-indent-ignore-first-tab t
+   js2-basic-indent 2
+   js2-basic-offset 2
+   js2-auto-indent-p t
+   js2-cleanup-whitespace t
+   js2-enter-indents-newline t
+   js2-indent-on-enter-key t
+   js2-global-externs (list "window" "module" "require" "assert" "expect" "jQuery" "$")))
 
-(mc/require-packages '( js2-refactor  web-beautify js-doc json-mode))
+(use-package json-mode
+  :commands json-mode
+  :bind (:map json-mode-map
+              ("<f12>" . json-mode-beautify)))
+
+;; (mc/require-packages '( js2-refactor  web-beautify js-doc json-mode))
 
 ;; (add-hook 'js2-mode-hook
 ;;           (lambda ()
