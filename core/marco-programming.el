46ab0b8a2408c3ebe873cc23ba7175825a4a5d86
Marco Cotrufo <marco.cotrufo@devncode.it>
2017-04-06 09:48:24 +0200

    For now disable the global-prettify-symbols-mode, see #3

--- core/marco-programming.el
+++ core/marco-programming.el
@@ -74,14 +74,14 @@
     (rainbow-delimiters-mode t))
   (add-hook 'prog-mode-hook 'mc/rainbow-mode-hook))
 
-(when (boundp 'global-prettify-symbols-mode)
-  (add-hook 'emacs-lisp-mode-hook
-            (lambda ()
-              (push '("lambda" . ?λ) prettify-symbols-alist)))
-  (add-hook 'js2-mode-hook
-            (lambda ()
-              (push '("function" . ?λ) prettify-symbols-alist)))
-  (global-prettify-symbols-mode +1))
+;; (when (boundp 'global-prettify-symbols-mode)
+;;   (add-hook 'emacs-lisp-mode-hook
+;;             (lambda ()
+;;               (push '("lambda" . ?λ) prettify-symbols-alist)))
+;;   (add-hook 'js2-mode-hook
+;;             (lambda ()
+;;               (push '("function" . ?λ) prettify-symbols-alist)))
+;;   (global-prettify-symbols-mode +1))
 
 (which-function-mode t)
 (electric-pair-mode t)
